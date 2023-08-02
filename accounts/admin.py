58d6eb4c7a7c1cff5b6d62ffc92485d2f7df5c6e
Jayant-saksham <Jayant2410@gmail.com>
2023-08-02 13:00:58 +0530

    Admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,9 +3,15 @@ from . models import User
 
 # Register your models here.
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'username', 'role','phone_number', 'is_active',)
+    list_display = ('email', 'username', 'role','phone_number', 'days_since_joined', 'is_active',)
     list_filter = ('created','is_staff')
     search_fields = ('email',)
+    readonly_fields = ('password',)
 
+    def days_since_joined(self, obj):
+        from django.utils import timezone
+        now = timezone.now()
+        return (now - obj.created).days
+    days_since_joined.short_description = 'Days since joined'
 
 admin.site.register(User, UserAdmin)
